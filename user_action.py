4b595fd11597200bd4e01501da719adf98394c63
NotJude <95127080+NotJude@users.noreply.github.com>
2025-09-03 17:04:31 +0200

    Delete user_action.py

--- user_action.py
+++
@@ -1,8 +0,0 @@
-class UserAction:
-    def __init__(self, sender):
-        self.sender = sender
-
-class PlayCard(UserAction):
-    def __init__(self, sender, card):
-        UserAction.__init__(self, sender)
-        self.card = card
