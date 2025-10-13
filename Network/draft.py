272042cabd5a3c17d87210582ad226ba44429750
NotJude <95127080+NotJude@users.noreply.github.com>
2025-10-13 14:03:28 +0200

    Delete Network/draft.py

--- Network/draft.py
+++
@@ -1,9 +0,0 @@
-for i in range(-5):
-    print(i)
-"""
-
-            full = self.client.recv(SERIALIZED_LENGTH).decode()
-            l_full = full.split('/')[1:-1]
-            m = full[0]
-            
-            """
